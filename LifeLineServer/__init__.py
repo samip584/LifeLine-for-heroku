627f60487fe4a8213f07b7cf5265e2b59ec60987
unknown <samip0884@gmail.com>
2020-05-10 17:48:33 +0545

    smth

--- LifeLineServer/__init__.py
+++ LifeLineServer/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = 'buzzgopa'
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+app.config['DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(basedir, 'db.sqlite')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
