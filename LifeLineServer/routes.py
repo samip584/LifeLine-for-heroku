b4deaa75f2c240356058c17428c7769a4293d0c0
samip584 <samip0884@gmail.com>
2021-02-13 13:58:18 +0545

    Addition of socket and all

--- LifeLineServer/routes.py
+++ LifeLineServer/routes.py
@@ -1,2 +1,12 @@
 from LifeLineServer.controllers.traffic import *
 from LifeLineServer.controllers.driver import *
+from LifeLineServer import driver_db, traffic_db
+
+
+# Get single traffics
+@app.route('/installDB', methods=['GET'])
+# @token_required
+def installDB():
+  driver_db.create_all()
+  traffic_db.create_all()
+  return jsonify({'message': 'DataBase initiated'})
