990d8a551ffe8822e81d97d972da635911869b66
samip584 <samip0884@gmail.com>
2021-03-06 13:26:49 +0545

    Fix cors

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from LifeLineServer import app, socket
 
 # Runserver
 if __name__ == "__main__":
-    socket.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
     # app.run(debug=True)
